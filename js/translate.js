06ff7e1270292d36617ce73781bea42b72041c80
randychoc <chocrandy@gmail.com>
2021-05-04 02:16:54 -0600

    Spanish - English

---
+++ js/translate.js
@@ -0,0 +1,14 @@
+var check = 'index.html';
+check = document.querySelector(".check");
+check.addEventListener('click', idioma);
+function idioma()
+{
+    let id = check.checked;
+    if (id == true) {
+        location.href="en/index.html";
+    }
+    else 
+    {
+        location.href="../index.html";
+    }
+}
